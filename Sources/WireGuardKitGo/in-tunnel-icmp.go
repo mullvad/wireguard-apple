43e7668c02238e374b16e1818fa3670e3f9c76f1
Andrew Bulhak <andrew.bulhak@mullvad.net>
2024-09-10 13:47:45 +0200

    factor ping response handling out to goroutine

--- Sources/WireGuardKitGo/in-tunnel-icmp.go
+++ Sources/WireGuardKitGo/in-tunnel-icmp.go
@@ -4,6 +4,7 @@ import "C"
 
 import (
 	"bytes"
+	"net"
 
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
@@ -37,6 +38,28 @@ func wgCloseInTunnelICMP(socketHandle int32) bool {
 	return ok
 }
 
+// returns the sequence number or an error code
+func parsePingResponse(socket *net.Conn, pingdata []byte) int {
+	readBuff := make([]byte, 1024)
+	readBytes, err := (*(socket)).Read(readBuff)
+	// it appears to be failing here sometimes in the app
+	if readBytes <= 0 || err != nil {
+		return errICMPReadSocket
+	}
+	replyPacket, err := icmp.ParseMessage(1, readBuff[:readBytes])
+	if err != nil {
+		return errICMPResponseFormat
+	}
+	replyPing, ok := replyPacket.Body.(*icmp.Echo)
+	if !ok {
+		return errICMPResponseFormat
+	}
+	if !bytes.Equal(replyPing.Data, pingdata) {
+		return errICMPResponseContent
+	}
+	return replyPing.Seq
+}
+
 // this returns the sequence number or a negative value if an error occurred
 //
 //export wgSendAndAwaitInTunnelPing
@@ -49,6 +72,26 @@ func wgSendAndAwaitInTunnelPing(tunnelHandle int32, socketHandle int32, sequence
 	pingdata := make([]byte, dataLength)
 	_, err := rng.Read(pingdata)
 	pingid := rng.Int()
+
+	resultChannel := make(chan int)
+	shutdownChannel := make(chan struct{})
+
+	// the reading goroutine
+	go func() {
+		for {
+			select {
+			case <-shutdownChannel:
+				return
+			default:
+			}
+			result := parsePingResponse(socket.icmpSocket, pingdata)
+			if result == errICMPResponseContent || result >= 0 {
+				resultChannel <- result
+				return
+			}
+		}
+	}()
+
 	// probably not worth checking for an error here
 	ping := icmp.Message{
 		Type: ipv4.ICMPTypeEcho,
@@ -65,22 +108,24 @@ func wgSendAndAwaitInTunnelPing(tunnelHandle int32, socketHandle int32, sequence
 	if err != nil {
 		return errICMPWriteSocket
 	}
-	readBuff := make([]byte, 1024)
-	readBytes, err := (*(socket.icmpSocket)).Read(readBuff)
-	// it appears to be failing here sometimes in the app
-	if readBytes <= 0 || err != nil {
-		return errICMPReadSocket
-	}
-	replyPacket, err := icmp.ParseMessage(1, readBuff[:readBytes])
-	if err != nil {
-		return errICMPResponseFormat
-	}
-	replyPing, ok := replyPacket.Body.(*icmp.Echo)
-	if !ok {
-		return errICMPResponseFormat
-	}
-	if replyPing.Seq != int(sequenceNumber) || !bytes.Equal(replyPing.Data, pingdata) {
-		return errICMPResponseContent
-	}
-	return int32(sequenceNumber)
+	defer close(shutdownChannel)
+	return int32(<-resultChannel)
+	// readBuff := make([]byte, 1024)
+	// readBytes, err := (*(socket.icmpSocket)).Read(readBuff)
+	// // it appears to be failing here sometimes in the app
+	// if readBytes <= 0 || err != nil {
+	// 	return errICMPReadSocket
+	// }
+	// replyPacket, err := icmp.ParseMessage(1, readBuff[:readBytes])
+	// if err != nil {
+	// 	return errICMPResponseFormat
+	// }
+	// replyPing, ok := replyPacket.Body.(*icmp.Echo)
+	// if !ok {
+	// 	return errICMPResponseFormat
+	// }
+	// if replyPing.Seq != int(sequenceNumber) || !bytes.Equal(replyPing.Data, pingdata) {
+	// 	return errICMPResponseContent
+	// }
+	// return int32(sequenceNumber)
 }
